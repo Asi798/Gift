bc71ab3f718bc668c6b641c3699e3d8f279c9cd1
Asi798 <120752666+Asi798@users.noreply.github.com>
2024-03-20 18:27:34 +0100

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -5,7 +5,52 @@ const datePicker = document.getElementById("datePicker");
 datePicker.addEventListener("change", function () {
   // Get the selected date value
   const selectedDate = datePicker.value;
+  const sources = [
+    "https://gifdb.com/images/high/kawaii-cat-cheerful-dance-7hpalfa53geygxqa.gif",
+    "https://media.tenor.com/lCKwsD2OW1kAAAAi/happy-cat-happy-happy-cat.gif",
+    "https://media.tenor.com/1jrUvITt1QcAAAAi/github-sticker.gif",
+    "https://media.tenor.com/1MGcyWdEnmEAAAAi/cutecats-kitten.gif",
+    "https://media.tenor.com/Rrl0kEdmhl0AAAAj/amazed-sparkle.gif",
+    "https://media.tenor.com/Ex1pkci_-v8AAAAj/white-cute-cat-hearts.gif",
+    "https://media.tenor.com/9X_EXRhnImUAAAAj/love-ilu.gif",
+    "https://media.tenor.com/DZKcXvpu-d8AAAAi/bongo-cat-cute-png.gif",
+    "https://media.tenor.com/gBRGMDDg6z4AAAAj/peach-peach-and-goma.gif",
+    "https://media.tenor.com/7Ie2NGeUuFgAAAAj/cutecat.gif",
+    "https://media.tenor.com/s-Nnz3VDs5sAAAAj/ilyakazakov-heart.gif",
+    "https://media.tenor.com/hnpqa8Zp8z8AAAAj/mochi-kiss.gif",
+    "https://media.tenor.com/guk9njDOl3AAAAAj/peachcat-cat.gif",
+  ];
+  let sec = 65;
+  let timer = setInterval(function () {
+    sec--;
+    console.log(timer);
+    if (sec < 0) {
+      clearInterval(timer);
+    } else {
+      if (sec % 5 == 0) {
+        let image = document.createElement("img");
+        let randomIndex = Math.floor(Math.random() * sources.length);
+        // Set the source of the image to a random URL from sources
+        image.src = sources[randomIndex];
+        image.style.zIndex = "9999";
 
+        // Calculate random positions inside the visible area of the screen
+        let maxX = 200;
+        let maxY = 700;
+        let randomX = Math.floor(Math.random() * maxX);
+        let randomY = Math.floor(Math.random() * maxY);
+
+        // Set the position of the image
+        image.style.maxWidth = "200px";
+        image.style.position = "fixed";
+        image.style.top = randomY + "px";
+        image.style.left = randomX + "px";
+
+        document.body.appendChild(image);
+        sources.splice(randomIndex, 1);
+      }
+    }
+  }, 1000);
   // Perform your desired action here, for example:
   let image = document.createElement("img");
   let musica = document.createElement("audio");
